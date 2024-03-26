f66c6e6b771b7c495ea9654a11c8d4d2722f3eca
Vanessa Gaete <48606635+VanessaGaete@users.noreply.github.com>
2024-03-26 01:02:31 -0300

    Update and rename elec_pres_script.py to elec_pres_querys.py

--- elec_pres_querys.py
+++ elec_pres_querys.py
@@ -6,15 +6,10 @@ import dictionaries
 
 
 df1 = pd.read_csv('exp2_inputs_filtrado.csv')
-#df1 = df1.drop(df1.index[:298])
-#data = pd.read_csv('exp1_inputs.csv')
 fewshot_df = pd.read_csv('./prompt-dataframes/prompt_dataset_presidencial_fewshot.csv')
 fewshotexamples_df = pd.read_csv('./prompt-dataframes/prompt_dataset_presidencial_fewshotexamples.csv')
 
-
-# Definir la clase derivada (clase hija) que hereda de Animal
 class Elecpres_Requester(Exp_Request):
-  #model,
   def __init__(self, model, b, name, sys=False, eng=False, red=False, wpv=False,with_context = True, spec = False,translate= False, ablation = False, fewshot = False, finetuned=False, random_fewshot =False, nsamples = 0):
     # Llama al constructor de la clase padre usando super()
     #model_llama
@@ -28,15 +23,10 @@ class Elecpres_Requester(Exp_Request):
     jsonl_name = './' + model + '_elecpres_' + b + 'b' +name + '.jsonl'
     if finetuned:
       jsonl_name = './elecpres' +name + '.jsonl'
-    #jsonl_name = './respuestas_elecpres_' + b + 'b' +name + '.jsonl'
     
     #####################   Prompts   ######################
-    #if finetuned:
-    #  task= "Analiza el perfil de la siguiente persona y entrega la respuesta que probablemente daría dicha persona a la pregunta que se hará.\n "
-      #inst = ""
-    #  end= ""
+  
     if not eng:
-      #task="Tu trabajo es ser un asistente que ayudará a predecir el voto de ciertas personas en la segunda vuelta presidencial de Chile del 2021. Debes responder siempre con tan solo una palabra de entre las siguientes opciones: Boric, Kast o Nulo.\n "
       task = "Eres un modelo predictor. Tu tarea es predecir las respuestas de una persona a cierta pregunta basándote en sus características sociodemográficas. Las características sociodemográficas estarán delimitadas con triple backsticks, la pregunta estará delimitada con los siguientes símbolos < >, y las alternativas de respuesta por [ ]."
       context = "Primero te voy a dar un poco de contexto en el que debes situarte. La elección presidencial de Chile para el período 2022-2026 se realizó el 21 de noviembre de 2021, la segunda vuelta electoral, en tanto, tuvo lugar el 19 de diciembre de 2021.\n El resultado de la primera vuelta presidencial ubica a José Antonio Kast en primer lugar con un 27,91% de los votos, a Gabriel Boric Font en segundo lugar con un 25.83% de los votos, el resto de los candidatos no lograron obtener más de un 12% de los votos. Por lo tanto, Gabriel Boric y José Antonio Kast fueron los candidatos que pasaron a la segunda vuelta.\n Así los chilenos debían elegir entre un candidato de izquierda y otro de derecha respectivamente. El enfoque principal de la candidatura de Gabriel Boric era igualdad de género, seguridad social y sustentabilidad. Por otro lado el candidato José Antonio Kast realizó una campaña centrada en seguridad, orden, disminución del Estado e instalándose como opositor a los sucesos relacionados con el estallido social del 2019.\n"
       end_JSON = "Genera solo un JSON (sin palabras ni explicaciones extras) con las siguientes llaves: 'prediccion' que muestre solo la opción elegida de entre las alternativas que se te darán, y 'explicacion' (en máximo 100 palabras) conteniendo la razón por la que elegiste dicha alternativa."
@@ -54,24 +44,12 @@ class Elecpres_Requester(Exp_Request):
     if not with_context:
       context = ""
 
-    super().__init__(1, model, b, jsonl_name,  task, context, end , inst, dictionary, sys,eng, red, wpv,translate, ablation, fewshot, finetuned, nsamples, random_fewshot, examples_df, imatrix )
-    #super().__init__(1, model_name, jsonl_name,  task+context, end , inst, dictionary, eng, red, wpv, ablation, fewshot, nsamples, random_fewshot, examples_df, imatrix )
-              
+    super().__init__(1, model, b, jsonl_name,  task, context, end , inst, dictionary, sys,eng, red, wpv,translate, ablation, fewshot, finetuned, nsamples, random_fewshot, examples_df, imatrix )             
 
 ########################################### Función Requests ###################################################
 b = str(7)
 
 ################################################### Original ###################################################
-#r = Elecpres_Requester("mistral", "v0.2", "_sys", sys=True)
-#r.answers_generator(df1)
-
-#r=Elecpres_Requester("llama", str(7), "_final")
-#r.answers_generator(df1)
-
-#r=Elecpres_Requester("llama", str(13), "_final")
-#r.answers_generator(df1)
-
-#r=Elecpres_Requester("llama-2-13b-datacamp_v5_PRES_ENG",str(13), "A", finetuned = True)
 r=Elecpres_Requester("llama-2-13b-datacamp_v6_ENG_ONLYPRES",str(13), "13b_v6_ONLYPRES_ENG.jsonl", translate=True, finetuned = True)
 r.answers_generator(df1)
 
@@ -88,18 +66,13 @@ r.answers_generator(df1)
 ###################################################  WPV  ###################################################
 #answers_generator(dataset_wpv, b, task, context, end, inst, name = "_wpv")
 
-""" r = Elecpres_Requester("mistral", "v0.2", "_sys_wpv", sys=True)
-r.answers_generator(df1)
-
+"""
 r = Elecpres_Requester("mistral", "v0.2", "_wpv")
 r.answers_generator(df1)
  """
 
 ###################################################  REDUCED  ###################################################
-""" #answers_generator(dataset_red, b, task, context, end, inst, name = "_red")
-r = Elecpres_Requester("mistral", "v0.2", "_sys_red", sys=True)
-r.answers_generator(df1)
-
+""" 
 r = Elecpres_Requester("mistral", "v0.2", "_red")
 r.answers_generator(df1) """
 ###################################################  ENG  ###################################################
@@ -112,15 +85,9 @@ r.answers_generator(df1) """
 
 # r=Elecpres_Requester("respuestas", str(13), "_eng", eng =True)
 # r.answers_generator(df1)
-
-#r = Elecpres_Requester("mistral", "v0.2", "_eng_resto", eng= True)
-#r.answers_generator(df1)
 ############################################## WITHOUT CONTEXT ####################################
 # r = Elecpres_Requester("llama", b, "_withoutcontext", with_context = False)
 # r.answers_generator(df1) #3, 6 
-
-#r = Elecpres_Requester("mistral", "v0.2", "_sys_withoutcontext", sys=True, with_context = False )
-#r.answers_generator(df1)
 
 #r = Elecpres_Requester("mistral", "v0.2", "_withoutcontext", with_context = False)
 #r.answers_generator(df1) 
